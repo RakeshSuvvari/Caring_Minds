3e2e74883c0062a37590185e4065d7d754669d2d
Rakesh Suvvari <rakeshsuvvari@Rakeshs-Laptop.local>
2024-11-27 17:36:32 -0600

    routerapp

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+# Import your cosine algo function
+from CosineAlgo import match_caregivers
+app = Flask(__name__)
+CORS(app)
+
+# API route to match caregivers based on carerecipient requirements
+@app.route('/match_caregivers', methods=['POST'])
+def run_match_caregivers():
+  care_recipient_data = request.json
+  result = match_caregivers(care_recipient_data)
+  return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
